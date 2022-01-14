fd378ff5c4854588a18c1a963720542c4ef8f858
Shane Strachan <shane@Shanes-MacBook-Pro-2.local>
2022-01-14 00:40:41 -0800

    removed key and extraneous comments

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const url =
-  "https://api.covidactnow.org/v2/states.timeseries.json?apiKey=884a3832443f4ac39a71761cbedbb639";
+const url = "https://api.covidactnow.org/v2/states.timeseries.json?apiKey=";
 
 export const fetchData = async () => {
   try {
@@ -18,51 +17,3 @@ export const fetchData = async () => {
     }, {});
   } catch (error) {}
 };
-
-// export const fetchStates = async () => {
-//   try {
-//     // const {
-//     //   data: { states },
-//     // } = await axios.get(`${url}/states`);
-
-//     // return states.map((state) => state.name);
-
-//     return ["AK"];
-//   } catch (error) {}
-// };
-
-// export const fetchData = async () => {
-//   try {
-//     const { data } = await axios.get(url);
-
-//     const final = data
-//       .map((item) => item.state)
-//       .reduce((statesValue, state, index) => {
-//         return {
-//           ...statesValue,
-//           [state]: {
-//             deaths: data[index].actuals.newDeaths,
-//             icubeds: data[index].actuals.icuBeds.currentUsageCovid,
-//           },
-//         };
-//       }, {});
-
-//     return final;
-//   } catch (error) {}
-// };
-
-// async function __fetchData() {
-//   try {
-//     const { data } = await axios.get(url);
-
-//     return data.reduce((result, datum) => {
-//       return {
-//         ...result,
-//         [datum.state]: {
-//           deaths: datum.actuals.newDeaths,
-//           icubeds: datum.actuals.icuBeds.currentUsageCovid,
-//         },
-//       };
-//     }, {});
-//   } catch (error) {}
-// }
